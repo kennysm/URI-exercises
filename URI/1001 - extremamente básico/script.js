afe022b5ce633296ba6dbcfbb5abd86903a37444
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-08-18 21:41:31 -0300

    Delete URI/1001 - extremamente básico directory

--- URI/1001 - extremamente básico/script.js
+++
@@ -1,7 +0,0 @@
-var input = require('fs').readFileSync('stdin1', 'utf8');
-var PI = 3.14159;
-var raio = parseFloat(input);
-var area = PI * Math.pow(raio, 2);
-
-console.log("A=" + area.toFixed(4));
-
